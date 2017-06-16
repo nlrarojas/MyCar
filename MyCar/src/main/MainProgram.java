21367eb042410f5cf16c711a82a71af7813ad4a4
Yelson11 <yelsonmonge@gmail.com>
2017-06-16 09:44:31 -0600

    Prueba de commit

--- MyCar/src/main/MainProgram.java
+++ MyCar/src/main/MainProgram.java
@@ -1,8 +1,11 @@
 package main;
 
+import view.Dash;
+
 public class MainProgram {
     
     public static void main(String[] args) {
-        
+        Dash d = new Dash();
+        d.show();
     }
 }
