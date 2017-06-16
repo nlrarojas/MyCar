5c73ed8ff62b92a6935eec0e9b72364c73bf97a0
Yelson11 <yelsonmonge@gmail.com>
2017-06-15 23:27:08 -0600

    Lectura de archivo y manejo de hilos

--- MyCar/src/controller/RoadController.java
+++ MyCar/src/controller/RoadController.java
@@ -1,8 +1,23 @@
 package controller;
 
 public class RoadController {
+    
+    FileReader document;
+    String[] road;
 
     public RoadController() {
-
+        document = new FileReader();
+    }
+    
+    public String[] chargeRoad(){
+        document.readText("C:\\Users\\Yelson\\Documents\\GitHub\\MyCar\\MyCar\\Road.txt"); 
+        // Cualquier direccion, en estoy usando esta en este caso
+        String text = this.document.getText();
+        String[] list = text.split("");
+        return list;
+    }
+    public String[] getRoad(){
+        road = chargeRoad();
+        return road;
     }
 }
--- MyCar/src/controller/ThreadController.java
+++ MyCar/src/controller/ThreadController.java
@@ -1,8 +1,23 @@
 package controller;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
 public class ThreadController {
-
-    public ThreadController() {
-
+    private static ThreadController threadControl = null;
+    ExecutorService executorService = Executors.newFixedThreadPool(5);
+    
+    private ThreadController() {
+    }
+    //Asi solo se instancia una unica vez
+    public static ThreadController getInstance(){
+        if (threadControl == null){
+            threadControl = new ThreadController();
+        }
+        return threadControl;
+    }
+    
+    public ExecutorService getExecutor(){
+        return executorService;
     }
 }
