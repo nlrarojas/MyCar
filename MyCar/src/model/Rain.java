93e33b5580cb7305d00fc2db41fbce66ee0e0254
Yelson11 <yelsonmonge@gmail.com>
2017-06-19 09:56:47 -0600

    Imagenes de lluvia y velocidades

--- MyCar/src/model/Rain.java
+++ MyCar/src/model/Rain.java
@@ -5,7 +5,12 @@ public class Rain extends Obstacle{
     
     @Override
     public void generate(boolean b){
-        
+        if (b){
+            this.path = "src\\images\\carreteraDiaL\\carreteraDia";
+        }
+        else{
+            this.path = "src\\images\\carreteraNocheL\\carreteraNoche";
+        }
     }
 
     @Override
